2dd7a61a286e881effeca59fcfff475a02f3cf50
bogdan <bogdancupsa@yahoo.com>
2024-05-12 16:23:01 -0400

    added first someip functionality

--- src/services/someip/SomeipService.cpp
+++ src/services/someip/SomeipService.cpp
@@ -1,25 +1,41 @@
 #include "SomeipService.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-SomeipService::SomeipService (void) {
+void serialize_message (SomeIPMessage* deserialized_msg, uint8_t* serialized_msg)
+{
+
+    memcpy(&serialized_message, &deserialized_message.someip_header, sizeof(deserialized_message.someip_header));
+  
+    memcpy(&serialized_message + sizeof(deserialized_message.someip_header), &deserialized_message.someip_payload, sizeof(deserialized_message.someip_header.length));
 
 }
 
-void SomeipService::init (void) {
+
+void deserialize_message (uint8_t* serialized_msg, SomeIPMessage* deserialized_msg)
+{
+
+    memcpy(&deserialized_message.someip_header, &serialized_message, sizeof(deserialized_message.someip_header));
+
+    memcpy(&deserialized_message.someip_payload, &serialized_message + sizeof(deserialized_message.someip_header), message.header.length);
 
 }
 
-void SomeipService::start (void) {
-    
+void send_someip_msg (SomeIPMessage* message) 
+{
+    UDPSocket socket(12345);
+    uint8_t buffer[1024];
+
+    serialize_message(&message, &buffer);
+    socket.send("192.168.1.11", 12345, buffer);
 }
 
-void SomeipService::stop (void) {
-    
-}
+void receive_someip_msg (void)
+{
+    UDPSocket socket(12345);
+    uint8_t buffer[1024];
+    SomeIPMessage message;
 
-void SomeipService::offer_service (void) {
-    
-}
-
-void SomeipService::on_message (void) {
-    
+    socket.receive(buffer);
+    deserialize_message(&buffer, &message);
 }
