a9b45d64b0327259cbeddc8f5d036ec720c3a536
bogdan <bogdancupsa@yahoo.com>
2024-06-01 06:07:47 -0400

    integrated diagnosis over ip functionality

--- src/scheduler/Scheduler.cpp
+++ src/scheduler/Scheduler.cpp
@@ -3,6 +3,7 @@
 // #include "CanService.h"
 #include "Tasks.h"
 #include "udpSock.h"
+#include "DoIP.h"
 
 void Scheduler::initialize (void) 
 {  
@@ -13,14 +14,25 @@ void Scheduler::initialize (void)
 
 void Scheduler::run (void) 
 {
-    // UDPSocket sendSocket = UDPSocket(12345);
-    
+
+#if TEST_SESSION_ACTIVE == 0
+
     SomeIPMessage request_msg;
     request_msg.someip_header.message_id = 1;
     request_msg.someip_header.length = 0;
     request_msg.someip_header.protocol_version = 1;
     request_msg.someip_header.message_type = REQUEST;
     request_msg.someip_header.return_code = 0;
+
+#else
+
+    DoIPMessage request_msg;
+    request_msg.doip_header.protocol_version = 0x02;
+    request_msg.doip_header.inverse_protocol_version = 0xFD;
+    request_msg.doip_header.payload_type = 0x0003; /*VIN*/
+    request_msg.doip_header.payload_length = 0;
+
+#endif
     
     while (!exitCondition) 
     {
@@ -34,6 +46,8 @@ void Scheduler::run (void)
             execute10msTask();
         }
 
+#if TEST_SESSION_ACTIVE == 0
+
         send_someip_msg(&request_msg, "192.168.1.11", 12345);
 
         SomeIPMessage response_msg = receive_someip_msg();
@@ -42,6 +56,19 @@ void Scheduler::run (void)
         {
             printf("Pedal status: %s", response_msg.someip_payload);
         }
+#else
+
+        /* send request */
+        send_doip(&request_msg, "192.168.1.11", 13400);
+
+        /* get response */
+        DoIPMessage response_msg = receive_doip(13401);
+        if (0x0004 == response_msg.doip_header.payload_type)
+        {
+            std::cout << "received vin" << std::endl;
+        }
+
+#endif
 
     }
 
